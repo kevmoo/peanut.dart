132aec66126f0bce97008401888aafc0736d91a2
Kevin Moore <kevmoo@google.com>
2015-04-08 11:24:25 -0700

    calling it 0.0.1

--- bin/peanut.dart
+++ bin/peanut.dart
@@ -1,11 +1,39 @@
 library peanut.bin;
 
+import 'dart:io';
+
+import 'package:args/args.dart';
 import 'package:peanut/peanut.dart';
 
-const _targetBranch = 'gh-pages';
-const _targetDir = 'web';
-const _commitMsg = 'building!';
+const _directoryFlag = 'directory';
+const _messageFlag = 'message';
 
-void main() {
-  run(_targetDir, _targetBranch, _commitMsg);
+void main(List<String> args) {
+  ArgParser parser = new ArgParser(allowTrailingOptions: false)
+    ..addOption(_directoryFlag, abbr: 'd', defaultsTo: 'web')
+    ..addOption('branch', abbr: 'b', defaultsTo: 'gh-pages')
+    ..addOption(_messageFlag, abbr: 'm', defaultsTo: 'Built <$_directoryFlag>')
+    ..addFlag('help', abbr: 'h', negatable: false);
+
+  var result = parser.parse(args);
+
+  if (result['help']) {
+    print(parser.usage);
+    exit(1);
+  }
+
+  if (result.rest.isNotEmpty) {
+    print(parser.usage);
+    exit(1);
+  }
+
+  var dir = result[_directoryFlag];
+  var branch = result['branch'];
+
+  var message = result[_messageFlag];
+  if (message == parser.getDefault(_messageFlag)) {
+    message = "Built $dir";
+  }
+
+  run(dir, branch, message);
 }
