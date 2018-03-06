275abc3bdd72e7e03acaab169f924dd04797a073
Kevin Moore <kevmoo@google.com>
2018-03-06 09:05:49 -0800

    Built example

---
+++ trivial_code.dart.js
@@ -0,0 +1,161 @@
+(function(){var supportsDirectProtoAccess=function(){var z=function(){}
+z.prototype={p:{}}
+var y=new z()
+if(!(y.__proto__&&y.__proto__.p===z.prototype.p))return false
+try{if(typeof navigator!="undefined"&&typeof navigator.userAgent=="string"&&navigator.userAgent.indexOf("Chrome/")>=0)return true
+if(typeof version=="function"&&version.length==0){var x=version()
+if(/^\d+\.\d+\.\d+\.\d+$/.test(x))return true}}catch(w){}return false}()
+function map(a){a=Object.create(null)
+a.x=0
+delete a.x
+return a}var A=map()
+var B=map()
+var C=map()
+var D=map()
+var E=map()
+var F=map()
+var G=map()
+var H=map()
+var J=map()
+var K=map()
+var L=map()
+var M=map()
+var N=map()
+var O=map()
+var P=map()
+var Q=map()
+var R=map()
+var S=map()
+var T=map()
+var U=map()
+var V=map()
+var W=map()
+var X=map()
+var Y=map()
+var Z=map()
+function I(){}init()
+function setupProgram(a,b,c){"use strict"
+function processStatics(a3,a4){var h=Object.keys(a3)
+for(var g=0;g<h.length;g++){var f=h[g]
+if(f==="^")continue
+var e=a3[f]
+var d=f.charCodeAt(0)
+var a0
+if(d===43){w[a0]=f.substring(1)
+var a1=a3[f]
+if(a1>0)a3[a0].$reflectable=a1
+if(e&&e.length)init.typeInformation[a0]=e}else if(d===42){n[a0].$D=e
+var a2=a3.$methodsWithOptionalArguments
+if(!a2)a3.$methodsWithOptionalArguments=a2={}
+a2[f]=a0}else if(typeof e==="function"){n[a0=f]=e
+i.push(f)
+init.globalFunctions[f]=e}else if(e.constructor===Array){}else{}}}var z=0
+if(!init.libraries)init.libraries=[]
+if(!init.mangledNames)init.mangledNames=map()
+if(!init.mangledGlobalNames)init.mangledGlobalNames=map()
+if(!init.statics)init.statics=map()
+if(!init.typeInformation)init.typeInformation=map()
+if(!init.globalFunctions)init.globalFunctions=map()
+var y=init.libraries
+var x=init.mangledNames
+var w=init.mangledGlobalNames
+var v=Object.prototype.hasOwnProperty
+var u=a.length
+var t=map()
+t.collected=map()
+t.pending=map()
+t.constructorsList=[]
+t.combinedConstructorFunction="function $reflectable(fn){fn.$reflectable=1;return fn};\n"+"var $desc;\n"
+for(var s=0;s<u;s++){var r=a[s]
+var q=r[0]
+var p=r[1]
+var o=r[2]
+var n=r[3]
+var m=r[4]
+var l=!!r[5]
+var k=m&&m["^"]
+if(k instanceof Array)k=k[0]
+var j=[]
+var i=[]
+processStatics(m,t)
+y.push([q,p,j,i,o,k,l,n])}}I.a=function(){}
+var dart=[["","",,H,{"^":"",
+c:function(a){if(typeof dartPrint=="function"){dartPrint(a)
+return}if(typeof console=="object"&&typeof console.log!="undefined"){console.log(a)
+return}if(typeof window=="object")return
+if(typeof print=="function"){print(a)
+return}throw"Unable to print message: "+String(a)}}],["","",,K,{"^":"",
+b:function(){H.c("Hello!")
+return}},1]]
+setupProgram(dart,0,0)
+var $=I.p
+$=null
+init.isHunkLoaded=function(a){return!!$dart_deferred_initializers$[a]}
+init.deferredInitialized=new Object(null)
+init.isHunkInitialized=function(a){return init.deferredInitialized[a]}
+init.initializeLoadedHunk=function(a){var z=$dart_deferred_initializers$[a]
+if(z==null)throw"DeferredLoading state error: code with hash '"+a+"' was not loaded"
+z($globals$,$)
+init.deferredInitialized[a]=true}
+init.deferredLibraryParts={}
+init.deferredPartUris=[]
+init.deferredPartHashes=[]
+I=I.$finishIsolateConstructor(I)
+$=new I()
+init.metadata=[]
+init.types=[]
+function convertToFastObject(a){function MyClass(){}MyClass.prototype=a
+new MyClass()
+return a}function convertToSlowObject(a){a.__MAGIC_SLOW_PROPERTY=1
+delete a.__MAGIC_SLOW_PROPERTY
+return a}A=convertToFastObject(A)
+B=convertToFastObject(B)
+C=convertToFastObject(C)
+D=convertToFastObject(D)
+E=convertToFastObject(E)
+F=convertToFastObject(F)
+G=convertToFastObject(G)
+H=convertToFastObject(H)
+J=convertToFastObject(J)
+K=convertToFastObject(K)
+L=convertToFastObject(L)
+M=convertToFastObject(M)
+N=convertToFastObject(N)
+O=convertToFastObject(O)
+P=convertToFastObject(P)
+Q=convertToFastObject(Q)
+R=convertToFastObject(R)
+S=convertToFastObject(S)
+T=convertToFastObject(T)
+U=convertToFastObject(U)
+V=convertToFastObject(V)
+W=convertToFastObject(W)
+X=convertToFastObject(X)
+Y=convertToFastObject(Y)
+Z=convertToFastObject(Z)
+function init(){I.p=Object.create(null)
+init.allClasses=map()
+init.getTypeFromName=function(a){return init.allClasses[a]}
+init.interceptorsByTag=map()
+init.leafTags=map()
+init.finishedClasses=map()
+I.$finishIsolateConstructor=function(a){var z=a.p
+function Isolate(){var y=Object.keys(z)
+for(var x=0;x<y.length;x++){var w=y[x]
+this[w]=z[w]}var v=init.lazies
+var u=v?Object.keys(v):[]
+for(var x=0;x<u.length;x++)this[v[u[x]]]=null
+function ForceEfficientMap(){}ForceEfficientMap.prototype=this
+new ForceEfficientMap()
+for(var x=0;x<u.length;x++){var t=v[u[x]]
+this[t]=z[t]}}Isolate.prototype=a.prototype
+Isolate.prototype.constructor=Isolate
+Isolate.p=z
+Isolate.a=a.a
+return Isolate}}(function(a){if(typeof document==="undefined"){a(null)
+return}if(typeof document.currentScript!='undefined'){a(document.currentScript)
+return}var z=document.scripts
+function onLoad(b){for(var x=0;x<z.length;++x)z[x].removeEventListener("load",onLoad,false)
+a(b.target)}for(var y=0;y<z.length;++y)z[y].addEventListener("load",onLoad,false)})(function(a){init.currentScript=a
+if(typeof dartMainRunner==="function")dartMainRunner(K.b,[])
+else K.b([])})})()
