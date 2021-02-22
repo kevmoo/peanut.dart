9c846efe8b80a6332201428acc8844c9afe6edb2
Kevin Moore <kevmoo@google.com>
2021-02-22 10:25:47 -0800

    Built example
    Branch: master
    Commit: b70e520f2e6c4979951382e5d3aea235587a1970
    
    package:peanut 3.7.0

--- example_script.dart.js
+++ example_script.dart.js
@@ -1,6 +1,8 @@
-{}(function dartProgram(){function copyProperties(a,b){var u=Object.keys(a)
+(function dartProgram(){function copyProperties(a,b){var u=Object.keys(a)
 for(var t=0;t<u.length;t++){var s=u[t]
-b[s]=a[s]}}var z=function(){var u=function(){}
+b[s]=a[s]}}function mixinProperties(a,b){var u=Object.keys(a)
+for(var t=0;t<u.length;t++){var s=u[t]
+if(!b.hasOwnProperty(s))b[s]=a[s]}}var z=function(){var u=function(){}
 u.prototype={p:{}}
 var t=new u()
 if(!(t.__proto__&&t.__proto__.p===u.prototype.p))return false
@@ -12,20 +14,29 @@ for(var u=0;u<a.length;u++){var t=a[u]
 var s=Object.keys(t)
 for(var r=0;r<s.length;r++){var q=s[r]
 var p=t[q]
-if(typeof p=='function')p.name=q}}}function inherit(a,b){a.prototype.constructor=a
+if(typeof p=="function")p.name=q}}}function inherit(a,b){a.prototype.constructor=a
 a.prototype["$i"+a.name]=a
 if(b!=null){if(z){a.prototype.__proto__=b.prototype
 return}var u=Object.create(b.prototype)
 copyProperties(a.prototype,u)
-a.prototype=u}}function inheritMany(a,b){for(var u=0;u<b.length;u++)inherit(b[u],a)}function mixin(a,b){copyProperties(b.prototype,a.prototype)
-a.prototype.constructor=a}function lazy(a,b,c,d){var u=a
+a.prototype=u}}function inheritMany(a,b){for(var u=0;u<b.length;u++)inherit(b[u],a)}function mixin(a,b){mixinProperties(b.prototype,a.prototype)
+a.prototype.constructor=a}function lazyOld(a,b,c,d){var u=a
 a[b]=u
 a[c]=function(){a[c]=function(){H.e(b)}
 var t
 var s=d
 try{if(a[b]===u){t=a[b]=s
 t=a[b]=d()}else t=a[b]}finally{if(t===s)a[b]=null
-a[c]=function(){return this[b]}}return t}}function makeConstList(a){a.immutable$list=Array
+a[c]=function(){return this[b]}}return t}}function lazy(a,b,c,d){var u=a
+a[b]=u
+a[c]=function(){if(a[b]===u)a[b]=d()
+a[c]=function(){return this[b]}
+return a[b]}}function lazyFinal(a,b,c,d){var u=a
+a[b]=u
+a[c]=function(){if(a[b]===u){var t=d()
+if(a[b]!==u)H.K(b)
+a[b]=t}a[c]=function(){return this[b]}
+return a[b]}}function makeConstList(a){a.immutable$list=Array
 a.fixed$length=Array
 return a}function convertToFastObject(a){function t(){}t.prototype=a
 new t()
@@ -35,7 +46,7 @@ return d?function(){if(u===null)u=H.u(this,a,b,c,true,false,e).prototype
 return u}:tearOffGetter(a,b,c,e,f)}var x=0
 function installTearOff(a,b,c,d,e,f,g,h,i,j){var u=[]
 for(var t=0;t<h.length;t++){var s=h[t]
-if(typeof s=='string')s=a[s]
+if(typeof s=="string")s=a[s]
 s.$callName=g[t]
 u.push(s)}var s=u[0]
 s.$R=e
@@ -55,31 +66,41 @@ var t=u.length
 u.push.apply(u,a)
 return t}function updateHolder(a,b){copyProperties(b,a)
 return a}var hunkHelpers=function(){var u=function(a,b,c,d,e){return function(f,g,h,i){return installInstanceTearOff(f,g,a,b,c,d,[h],i,e)}},t=function(a,b,c,d){return function(e,f,g,h){return installStaticTearOff(e,f,a,b,c,[g],h,d)}}
-return{inherit:inherit,inheritMany:inheritMany,mixin:mixin,installStaticTearOff:installStaticTearOff,installInstanceTearOff:installInstanceTearOff,_instance_0u:u(0,0,null,["$0"],0),_instance_1u:u(0,1,null,["$1"],0),_instance_2u:u(0,2,null,["$2"],0),_instance_0i:u(1,0,null,["$0"],0),_instance_1i:u(1,1,null,["$1"],0),_instance_2i:u(1,2,null,["$2"],0),_static_0:t(0,null,["$0"],0),_static_1:t(1,null,["$1"],0),_static_2:t(2,null,["$2"],0),makeConstList:makeConstList,lazy:lazy,updateHolder:updateHolder,convertToFastObject:convertToFastObject,setFunctionNamesIfNecessary:setFunctionNamesIfNecessary,updateTypes:updateTypes,setOrUpdateInterceptorsByTag:setOrUpdateInterceptorsByTag,setOrUpdateLeafTags:setOrUpdateLeafTags}}()
+return{inherit:inherit,inheritMany:inheritMany,mixin:mixin,installStaticTearOff:installStaticTearOff,installInstanceTearOff:installInstanceTearOff,_instance_0u:u(0,0,null,["$0"],0),_instance_1u:u(0,1,null,["$1"],0),_instance_2u:u(0,2,null,["$2"],0),_instance_0i:u(1,0,null,["$0"],0),_instance_1i:u(1,1,null,["$1"],0),_instance_2i:u(1,2,null,["$2"],0),_static_0:t(0,null,["$0"],0),_static_1:t(1,null,["$1"],0),_static_2:t(2,null,["$2"],0),makeConstList:makeConstList,lazy:lazy,lazyFinal:lazyFinal,lazyOld:lazyOld,updateHolder:updateHolder,convertToFastObject:convertToFastObject,setFunctionNamesIfNecessary:setFunctionNamesIfNecessary,updateTypes:updateTypes,setOrUpdateInterceptorsByTag:setOrUpdateInterceptorsByTag,setOrUpdateLeafTags:setOrUpdateLeafTags}}()
 function initializeDeferredHunk(a){x=v.types.length
 a(hunkHelpers,v,w,$)}function getGlobalFromName(a){for(var u=0;u<w.length;u++){if(w[u]==null)continue
-if(w[u][a])return w[u][a]}}var P={a:function a(){},K:function K(){}},H={
+if(w[u][a])return w[u][a]}}var P={c:function c(){},a:function a(){}},H={
 q:function(a){if(typeof dartPrint=="function"){dartPrint(a)
 return}if(typeof console=="object"&&typeof console.log!="undefined"){console.log(a)
 return}if(typeof window=="object")return
 if(typeof print=="function"){print(a)
-return}throw"Unable to print message: "+String(a)}},X={
+return}throw"Unable to print message: "+String(a)},
+x:function(a,b){return H.I(a.tR,b)},
+F:function(a,b){return H.I(a.eT,b)},
+I:function(a,b){var u,t,s=Object.keys(b),r=s.length
+for(u=0;u<r;++u){t=s[u]
+a[t]=b[t]}}},X={
 E:function(){H.q("Hello!")
-return}}
+return null}}
 var w=[P,H,X]
 hunkHelpers.setFunctionNamesIfNecessary(w)
 var $={}
+P.c.prototype={}
 P.a.prototype={constructor:P.a,$ia:1,
-toString:function(){return this.Z(this)}}
-P.K.prototype={};(function inheritance(){var u=hunkHelpers.inherit
+toString:function(){return this.Z(this)}};(function inheritance(){var u=hunkHelpers.inherit
 u(P.a,null)
-u(P.K,P.a)})()
-var v={mangledGlobalNames:{I:"int",C:"double",F:"num",K:"String",M:"bool",c:"Null",z:"List"},mangledNames:{},getTypeFromName:getGlobalFromName,metadata:[],types:[]}
+u(P.c,P.a)})()
+var v={typeUniverse:{eC:new Map(),tR:{},eT:{},tPV:{},sEA:[]},mangledGlobalNames:{B:"int",C:"double",l:"num",j:"String",M:"bool",c:"Null",z:"List"},mangledNames:{},getTypeFromName:getGlobalFromName,metadata:[],types:[],arrayRti:typeof Symbol=="function"&&typeof Symbol()=="symbol"?Symbol("$ti"):"$ti"}
+H.x(v.typeUniverse,JSON.parse('{}'))
+0
+0;(function nativeSupport(){hunkHelpers.setOrUpdateInterceptorsByTag({})
+hunkHelpers.setOrUpdateLeafTags({})})()
 convertAllToFastObject(w)
 convertToFastObject($);(function(a){if(typeof document==="undefined"){a(null)
-return}if(typeof document.currentScript!='undefined'){a(document.currentScript)
+return}if(typeof document.currentScript!="undefined"){a(document.currentScript)
 return}var u=document.scripts
 function onLoad(b){for(var s=0;s<u.length;++s)u[s].removeEventListener("load",onLoad,false)
 a(b.target)}for(var t=0;t<u.length;++t)u[t].addEventListener("load",onLoad,false)})(function(a){v.currentScript=a
-if(typeof dartMainRunner==="function")dartMainRunner(X.E,[])
-else X.E([])})})()
+var u=X.E
+if(typeof dartMainRunner==="function")dartMainRunner(u,[])
+else u([])})})()
